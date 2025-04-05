ca1c7b24d53ffe159ae567f7b3837b17a3cb5ef2
DejavuForensics <142698728+DejavuForensics@users.noreply.github.com>
2025-04-05 19:57:26 -0300

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+if [ $(id -u) -ne 0 ]
+  then echo Please run this script as root or using sudo!
+  exit
+fi
+
+apt install python3.11-venv
+python -m venv venv
+source venv/bin/activate
+
+pip install libsvm
+pip install brisque
+pip install scikit-learn
