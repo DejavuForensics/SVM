b60df8168c05257c7d0f28842c15863ac3900269
nltt-br <nlargato@gmail.com>
2025-04-06 08:01:56 -0400

    👽

--- load_virtual_environment.sh
+++ load_virtual_environment.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ $(id -u) -ne 0 ]
   then echo Please run this script as root or using sudo!
   exit
 fi
 
-apt install python3-virtualenv
+apt install update && apt install python3-virtualenv
 
 if [ -d "env" ]; then
   echo "Directory env already exists"
@@ -13,5 +13,3 @@ else
   python -m venv env
   source env/bin/activate
 fi
-
-
